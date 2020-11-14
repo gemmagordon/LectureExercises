9e932655bf3359ac763f49286480ccc6051ef509
gemmagordon <gemmagordon2012@gmail.com>
2020-11-14 08:51:10 +0000

    Lecture 16 script to try new push method

---
+++ lecture16_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3           
+import re 
+accessions = [
+  'xkn59438', 
+  'yhdck2', 
+  'eihd39d9', 
+  'chdsye847', 
+  'hedle3455', 
+  'xjhd53e', 
+  '45da', 
+  'de37dp']
+for acc in accessions:
+    print(acc)
+
+# this is a test script to try new git push method 
